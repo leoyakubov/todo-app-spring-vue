ea327ecf39158f05f949664694d1ed1f214d299a
leoyakubov <leoyakubovdev@gmail.com>
2023-04-12 21:49:12 +0300

    added frontend components for todos

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,101 +1,56 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated class="glossy">
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          @click="leftDrawerOpen = !leftDrawerOpen"
-          aria-label="Menu"
-          icon="menu"
-        />
-
         <q-toolbar-title>
-          Quasar App
+          <q-avatar>
+            <q-icon name="kayaking" size="30px"></q-icon>
+          </q-avatar>
+          Todo App
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        {{ this.claims && this.claims.email ? claims.email : '' }}
+        <q-btn flat round dense icon="logout" v-if='authState && authState.isAuthenticated' @click="logout"/>
+        <q-btn flat round dense icon="account_circle" v-else @click="login"/>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      class="bg-grey-2"
-    >
-      <q-list>
-        <q-item-label header>Essential Links</q-item-label>
-        <q-item clickable tag="a" target="_blank" href="https://quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="school" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Docs</q-item-label>
-            <q-item-label caption>quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://github.com/quasarframework/">
-          <q-item-section avatar>
-            <q-icon name="code" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Github</q-item-label>
-            <q-item-label caption>github.com/quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://chat.quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="chat" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Discord Chat Channel</q-item-label>
-            <q-item-label caption>chat.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://forum.quasar.dev">
-          <q-item-section avatar>
-            <q-icon name="forum" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Forum</q-item-label>
-            <q-item-label caption>forum.quasar.dev</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-item clickable tag="a" target="_blank" href="https://twitter.com/quasarframework">
-          <q-item-section avatar>
-            <q-icon name="rss_feed" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Twitter</q-item-label>
-            <q-item-label caption>@quasarframework</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </q-drawer>
-
     <q-page-container>
-      <HelloWorld />
+      <router-view></router-view>
     </q-page-container>
+
   </q-layout>
 </template>
 
 <script>
-import { ref } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'LayoutDefault',
-
-  components: {
-    HelloWorld
-  },
-
-  setup () {
+  data: function () {
     return {
-      leftDrawerOpen: ref(false)
+      claims: null
     }
-  }
+  },
+  watch: {
+    'authState.isAuthenticated'() {
+      this.$log.debug(('watch triggered!'))
+      this.updateClaims()
+    }
+  },
+  created() {
+    this.updateClaims()
+  },
+  methods: {
+    async updateClaims() {
+      if (this.authState && this.authState.isAuthenticated) {
+        this.claims = await this.$auth.getUser()
+      }
+    },
+    async login() {
+      await this.$auth.signInWithRedirect({ originalUri: '/todos' })
+    },
+    async logout() {
+      await this.$auth.signOut()
+    }
+  },
 }
 </script>
